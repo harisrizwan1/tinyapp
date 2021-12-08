689ab3fabe765936f6d680df781b643eb53ebb5f
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-08 04:24:56 -0600

    made navbar show login and logout options

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 
@@ -26,9 +28,15 @@ app.get("/", (req, res) => {
   res.redirect('/urls');
 });
 
+// login handler
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect('/urls');
+});
+
 // homepage
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -36,12 +44,14 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   const currentURL = generateRandoString();
   urlDatabase[currentURL] = req.body.longURL;
-  res.redirect(`/urls/${currentURL}`);
+  const templateVars = {username: req.cookies["username"]};
+  res.redirect(`/urls/${currentURL}`, templateVars);
 });
 
 // page for creating new links
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 // request handler for redirecting to actual link
@@ -52,7 +62,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 // page for viewing a single url
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
